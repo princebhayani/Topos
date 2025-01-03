68e526297d986c38dd2ebe54a7284697641db8bc
princebhayani <princebhayani192003@gmail.com>
2025-01-03 23:18:01 +0530

    configure user route

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,17 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const connectToDb = require("./db/db.js"); 
+const userRoutes = require('./routes/user.routes');
 
 connectToDb();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res)=>{
     res.send("hello world");
 })
+app.use('/users', userRoutes);
 
 module.exports = app;
