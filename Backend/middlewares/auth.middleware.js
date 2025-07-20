bf8d049aa7afe107cf20bd76138389bfeb86c80c
princebhayani <princebhayani192003@gmail.com>
2025-07-20 15:23:33 +0530

    authorization done

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+const blackListTokenModel = require('../models/blacklistToken.model');
 
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
@@ -10,6 +10,10 @@ module.exports.authUser = async (req, res, next) => {
         return res.status(401).json({ message: 'Unauthorized' });
     }
 
+    const isBlacklisted = await blackListTokenModel.findOne({ token });
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
 
     try {
 
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -22,6 +22,6 @@ router.post('/login', [
 
 router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
 
-router.get('/logout', userController.logoutUser)
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 module.exports = router;
