fe4dbb6fe259668521f4453d07f5fc5625454b8b
princebhayani <princebhayani192003@gmail.com>
2025-08-08 17:45:06 +0530

    implement socket.io integration; add socket initialization and user/captain connection handling

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const app = require('./app.js');
 const port = process.env.PORT || 3000;
-
+const { initializeSocket } = require('./socket');
 
 const server = http.createServer(app);
+
+initializeSocket(server);
 
 server.listen(port,()=>{
     console.log(`Server is runnig on port ${port}`);
